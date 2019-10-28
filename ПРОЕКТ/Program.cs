99bf1cad01c9f69ade4c7fc76cf2f9ade04edcd0
kauktus <55836097+kauktus@users.noreply.github.com>
2019-10-28 16:36:36 +0500

    Add files via upload

---
+++ ПРОЕКТ/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ПРОЕКТ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 11;
+            Console.WriteLine(a + b);
+        }
+    }
+}
